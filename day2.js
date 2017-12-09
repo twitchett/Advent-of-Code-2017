b977fafb69ada37951ec3c495ea1f29849b34781
Tabitha Goodall <tg.twitchett@gmail.com>
2017-12-10 00:50:53 +0100

    file structure refactor

--- day2.js
+++ day2.js
@@ -6,7 +6,7 @@
 
 const fs = require('fs')
 
-fs.readFile('input.txt', 'utf8', (err, data) => {
+fs.readFile('resources/day2-input.txt', 'utf8', (err, data) => {
     if (err) {
         console.error("Error reading input file", err)
     }
